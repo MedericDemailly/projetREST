01ff600e705b79c7bbcc5daf00e2591d3e77118a
Guillaume Schlegel <guillaume.schlegel@etu.iut-tlse3.fr>
2023-03-10 10:49:19 +0100

    feat(apiLogin.php jwt_util.php):changement de clé secrète

---
+++ apiLogin.php
@@ -0,0 +1,51 @@
+<?php
+    require_once('jwt_utils.php');
+
+    /// Librairies éventuelles (pour la connexion à la BDD, etc.)
+    //include('mylib.php');
+
+    /// Paramétrage de l'entête HTTP (pour la réponse au Client)
+    header("Content-Type:application/json");
+
+    /// Identification du type de méthode HTTP envoyée par le client
+    $http_method = $_SERVER['REQUEST_METHOD'];
+    if($http_method !="POST"){
+        deliver_response(400,"Pas de méthode post : 1",null);
+    }else{
+        /// Cas de la méthode POST
+        $user = "user";
+        $mdp = "1234";
+        /// Récupération des données envoyées par le Client
+        $postedData = file_get_contents('php://input');
+        $values = json_decode($postedData,true);
+        if(!isset($values['user']) || !isset($values['mdp'])){
+            deliver_response(400,"Pas de user ou mdp : 2",null);
+            die;  
+        } else{
+            if($values['user']!=$user){
+                deliver_response(400,"Pas bon user : 3",null);
+                die;
+            }
+            if($values['mdp'] != $mdp){
+                deliver_response(400,"Pas bon mdp : 4",null);
+                die;
+            }  
+        deliver_response(201, "Votre message", ["token"=>generate_jwt(
+            ["alg"=>"SHA256","typ"=>"JWT"],
+            ["username"=>$user,"password"=>$mdp, "exp" => time() + 3600])
+        ]);
+        }
+    }
+    /// Envoi de la réponse au Client
+    function deliver_response($status, $status_message, $data){
+        /// Paramétrage de l'entête HTTP, suite
+        header("HTTP/1.1 $status $status_message");
+        /// Paramétrage de la réponse retournée
+        $response['status'] = $status;
+        $response['status_message'] = $status_message;
+        $response['data'] = $data;
+        /// Mapping de la réponse au format JSON
+        $json_response = json_encode($response);
+        echo $json_response;
+    }
+   ?>

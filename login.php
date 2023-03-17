89ae0c0bff895c3bcaaf262b03b9acfa3de7ebb0
MedericDemailly <med.demailly@gmail.com>
2023-03-17 10:26:31 +0100

    feat(login.php, styleLogin.css) : Mise en forme ecran connexion

--- login.php
+++ login.php
@@ -4,13 +4,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="styleLogin.css">
+    <title>Se connecter</title>
 </head>
 <body>
-    <form action="utilisateur/apiLogin.php" method="post">
-        <input type='text' name='user' placeholder='Entrer votre username'>
-        <input type='password' name='mdp' placeholder='Enter votre mot de passe'>
-        <input type='submit' name='envoyer' value='Valider'>
-    </form>
+<div class="container">
+      <h1>Connexion</h1>
+      <form action='./utilisateur/apiLogin.php' method='POST'>
+        <label for="username">Identifiant :</label>
+        <input type="text" id="username" name="username" placeholder="Entrez votre identifiant">
+
+        <label for="password">Mot de passe :</label>
+        <input type="password" id="password" name="password" placeholder="Entrez votre mot de passe">
+
+        <button type="submit">Se connecter</button>
+      </form>
+    </div>
 </body>
 </html>

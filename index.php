80c33d1c3d59def999802f4d6218d1019b0385b6
MedericDemailly <med.demailly@gmail.com>
2023-03-17 11:09:57 +0100

    feat(index, apiLogin): Verify token before accessing index

--- index.php
+++ index.php
@@ -5,8 +5,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <?php
+        session_start();
+        require_once('./utilisateur/jwt_utils.php');
+    ?>
 </head>
 <body>
-    <p> Connecté </p>
+    <?php
+        if(!is_jwt_valid($_SESSION['token'])){
+            echo 'TOKEN INVALIDE';
+            die();
+        }
+    ?>
+    <p> Texte </p>
 </body>
 </html>

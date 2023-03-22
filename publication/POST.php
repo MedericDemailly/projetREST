2c4c33da3b19826a176071161f0ad14aabfa9741
Ribot Leo <leo.ribot@etu.iut-tlse3.fr>
2023-03-22 14:59:44 +0100

    feat(METHOD) add new functionalities for publication

---
+++ publication/POST.php
@@ -0,0 +1,40 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    include_once '../config/dbConnection.php';
+    include_once '../model/publication.php';
+
+    $database = new Database();
+    $db= $database->getConnexion();
+
+    $publication = new \model\publication($db);
+    $postedData = file_get_contents('php://input');
+
+    $publication->idPublication = $postedData['idPublication'];
+    $publication->idUtilisateur = $postedData['idUtilisateur'];
+    $publication->contenu = $postedData['contenu'];
+
+    if($publication->POST()) {
+        deliver_response(200, "message", null);
+    } else {
+        http_response_code(405);
+        echo json_encode(["message" => "La methode n'est pas autorisee"]);
+    }
+}
+
+function deliver_response($status, $status_message, $data) {
+    /// Paramétrage de l'entête HTTP, suite
+    header("HTTP/1.1 $status $status_message");
+    /// Paramétrage de la réponse retournée
+    $response['status'] = $status;
+    $response['status_message'] = $status_message;
+    $response['data'] = $data;
+    /// Mapping de la réponse au format JSON
+    $json_response = json_encode($response);
+    echo $json_response;
+}

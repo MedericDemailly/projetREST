3d5190c362679fe11a04a4df3732c3735520d39f
MedericDemailly <med.demailly@gmail.com>
2023-03-29 14:28:09 +0200

    feat(DELETE, PATCH avis) : add method for avis

---
+++ avis/PATCH.php
@@ -0,0 +1,40 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: PATCH");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+if($_SERVER['REQUEST_METHOD'] == 'PATCH') {
+    include_once '../config/dbConnection.php';
+    include_once '../model/avis.php';
+
+    $database = new Database();
+    $db= $database->getConnexion();
+
+    $avis = new \model\avis($db);
+    $postedData = file_get_contents('php://input');
+    $postedData = json_decode($postedData,true);
+
+    $avis->aimer = $postedData['aimer'];
+    $avis->idPublication = $postedData['idPublication'];
+    $avis->idUtilisateur = $postedData['idUtilisateur'];
+
+    if($avis->PATCH()) {
+        deliver_response(200, "Done", null);
+    } else {
+        deliver_response(405, "Unauthorized method", NULL);
+    }
+}
+
+function deliver_response($status, $status_message, $data) {
+    /// Paramétrage de l'entête HTTP, suite
+    header("HTTP/1.1 $status $status_message");
+    /// Paramétrage de la réponse retournée
+    $response['status'] = $status;
+    $response['status_message'] = $status_message;
+    $response['data'] = $data;
+    /// Mapping de la réponse au format JSON
+    $json_response = json_encode($response);
+    echo $json_response;
+}

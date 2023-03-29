20365be9590dd799cef4007b5aab48610c34546f
Ribot Leo <leo.ribot@etu.iut-tlse3.fr>
2023-03-29 15:47:11 +0200

    fix(is_user_allowed)

--- config/service.php
+++ config/service.php
@@ -12,6 +12,7 @@
                             break;
                         } else{
                             deliver_response(401, "Token invalide ou permission refusé",NULL);
+                            break;
                         }
                     case"PATCH":
                         if(is_user_allowed()){
@@ -19,6 +20,7 @@
                             break;
                         } else{
                             deliver_response(401, "Token invalide ou permission refusé",NULL);
+                            break;
                         }   
                     case"POST":
                         if(is_user_allowed()){
@@ -26,6 +28,7 @@
                             break;
                         } else{
                             deliver_response(401, "Token invalide ou permission refusé",NULL);
+                            break;
                         }
                     default :
                         break;
@@ -35,7 +38,23 @@ function is_user_allowed(){
     $bearer = get_bearer_token();
     $tokenParts = explode('.', $bearer);
     $payload = base64_decode($tokenParts[1]);
-    $role = explode(',', $payload);
-    $validRole = ($role == "moderator" || $role == "publisher");
+    $role = json_decode($payload)->role;
+    if($role == "moderator" || $role == "publisher") {
+        $validRole = true;
+    } else {
+        $validRole = false;
+    }
     return (is_jwt_valid($bearer) && $validRole);
+}
+
+function deliver_response($status, $status_message, $data) {
+    /// Paramétrage de l'entête HTTP, suite
+    header("HTTP/1.1 $status $status_message");
+    /// Paramétrage de la réponse retournée
+    $response['status'] = $status;
+    $response['status_message'] = $status_message;
+    $response['data'] = $data;
+    /// Mapping de la réponse au format JSON
+    $json_response = json_encode($response);
+    echo $json_response;
 }

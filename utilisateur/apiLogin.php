ca46ca0d2159566956ace69e9562f7fc9eb8e5ac
Ribot Leo <leo.ribot@etu.iut-tlse3.fr>
2023-03-30 08:42:06 +0200

    refactoring apiLogin & index.php

--- utilisateur/apiLogin.php
+++ utilisateur/apiLogin.php
@@ -30,13 +30,11 @@
                 extract($row);
                 if ($utilisateur == $identifiant) {
                     if ($mdp == $motDePasse) {
-                        session_start();
                         $token=generate_jwt(
                             ["alg" => "SHA256", "typ" => "JWT"],
                             ["idUtilisateur" => $idUtilisateur, "role" => $role, "exp" => time() + 3600]
                         );
-                        $_SESSION['token']=$token;
-                        header('Location: ../login/loginVerify.php');
+                        deliver_response(200, "Connexion reussie", $token);
                         die();
                     } else {
                         deliver_response(400, "Mot de passe incorrect", null);
